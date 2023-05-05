1e8318dffaeaf6cbdc989738e001286a7391d310
hungpppiii <hungppp303@gmail.com>
2023-05-05 22:38:07 +0700

    fix bug

--- src/server.js
+++ src/server.js
@@ -29,23 +29,23 @@ initRoute(app);
 app.use(errorHandler);
 
 // Verify that the callback came from Facebook.
-function verifyRequestSignature(req, res, buf) {
-    var signature = req.headers["x-hub-signature-256"];
+// function verifyRequestSignature(req, res, buf) {
+//     var signature = req.headers["x-hub-signature-256"];
 
-    if (!signature) {
-        console.warn(`Couldn't find "x-hub-signature-256" in headers.`);
-    } else {
-        var elements = signature.split("=");
-        var signatureHash = elements[1];
-        var expectedHash = crypto
-            .createHmac("sha256", config.appSecret)
-            .update(buf)
-            .digest("hex");
-        if (signatureHash != expectedHash) {
-            throw new Error("Couldn't validate the request signature.");
-        }
-    }
-}
+//     if (!signature) {
+//         console.warn(`Couldn't find "x-hub-signature-256" in headers.`);
+//     } else {
+//         var elements = signature.split("=");
+//         var signatureHash = elements[1];
+//         var expectedHash = crypto
+//             .createHmac("sha256", config.appSecret)
+//             .update(buf)
+//             .digest("hex");
+//         if (signatureHash != expectedHash) {
+//             throw new Error("Couldn't validate the request signature.");
+//         }
+//     }
+// }
 
 app.listen(config.port, () => {
     console.log(`Chatbot listening on port http://localhost:${config.port}`)
